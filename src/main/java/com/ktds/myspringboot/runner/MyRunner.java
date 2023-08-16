9a0ab5d693136dfe08728d3113d4c4a3f3865b7c
myvega2k <vega2k@kakao.com>
2023-08-16 15:31:07 +0900

    @Profile add

--- src/main/java/com/ktds/myspringboot/runner/MyRunner.java
+++ src/main/java/com/ktds/myspringboot/runner/MyRunner.java
@@ -1,6 +1,8 @@
 package com.ktds.myspringboot.runner;
 
+import com.ktds.myspringboot.dto.Customer;
 import com.ktds.myspringboot.property.MyBootProperties;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -16,8 +18,13 @@ public class MyRunner implements ApplicationRunner {
 
     @Autowired
     MyBootProperties properties;
+
+    @Resource(name="myCustomer")
+    Customer customer;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Customer Bean " + customer);
         System.out.println("VM Argument = " + args.containsOption("foo")); //false
         System.out.println("Program Argument = " + args.containsOption("bar")); //true
         System.out.println("환경변수 myboot.name = " + name);
