1078cd8a0fddd60aeced91b23d17734124ecc16e
myvega2k <vega2k@kakao.com>
2023-08-16 16:56:59 +0900

    test add

--- src/test/java/com/ktds/myspringboot/LambdaTest.java
+++ src/test/java/com/ktds/myspringboot/LambdaTest.java
@@ -5,6 +5,15 @@ import org.junit.jupiter.api.Test;
 public class LambdaTest {
     @Test
     void runnable() {
-
+        //class MyRunnable implements Runnable
+        //Thread(new MyRunnable())
+        //Anonymous Inner class
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Anonymous Inner class");
+            }
+        });
+        t1.start();
     }
 }
