5dc2d1985702db1e703e8c0838bda05930db09c6
myvega2k <vega2k@kakao.com>
2023-08-16 15:24:56 +0900

    @Profile add

--- src/main/java/com/ktds/myspringboot/config/DevConfig.java
+++ src/main/java/com/ktds/myspringboot/config/DevConfig.java
@@ -3,10 +3,12 @@ package com.ktds.myspringboot.config;
 import com.ktds.myspringboot.dto.Customer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class DevConfig {
-    @Bean
+    @Bean("myCustomer")
     public Customer customer() {
         return Customer.builder() //CustomerBuilder
                 .name("DevMode")
--- src/main/java/com/ktds/myspringboot/config/ProdConfig.java
+++ src/main/java/com/ktds/myspringboot/config/ProdConfig.java
@@ -3,10 +3,12 @@ package com.ktds.myspringboot.config;
 import com.ktds.myspringboot.dto.Customer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("prod")
 public class ProdConfig {
-    @Bean
+    @Bean("myCustomer")
     public Customer customer() {
         return Customer.builder() //CustomerBuilder
                 .name("ProdMode")
