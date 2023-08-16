1570751e054c8d0bf0a53b43b3c5c2b9a435c6ae
myvega2k <vega2k@kakao.com>
2023-08-16 14:20:12 +0900

    MyRunner update

--- src/main/java/com/ktds/myspringboot/runner/MyRunner.java
+++ src/main/java/com/ktds/myspringboot/runner/MyRunner.java
@@ -1,14 +1,21 @@
 package com.ktds.myspringboot.runner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
+    @Value("${myboot.age}")
+    private int age;
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("VM Argument = " + args.containsOption("foo"));
-        System.out.println("Program Argument = " + args.containsOption("bar"));
+        System.out.println("VM Argument = " + args.containsOption("foo")); //false
+        System.out.println("Program Argument = " + args.containsOption("bar")); //true
+        System.out.println("환경변수 myboot.name = " + name);
+        System.out.println("환경변수 myboot.age = " + age);
     }
 }
