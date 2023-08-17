4b87afb4e83d4553c3240e59889e02cd317bf898
myvega2k <vega2k@kakao.com>
2023-08-17 14:57:55 +0900

    repository test add

---
+++ src/test/java/com/ktds/myspringboot/repository/AccountRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.ktds.myspringboot.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AccountRepositoryTest {
+    @Autowired
+    AccountRepository accountRepository;
+
+
+}
