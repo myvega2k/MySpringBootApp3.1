9a50073306273902b7657b3d27f81deceb6013bf
myvega2k <vega2k@kakao.com>
2023-08-17 17:05:23 +0900

    service update

--- src/main/java/com/ktds/myspringboot/service/UserService.java
+++ src/main/java/com/ktds/myspringboot/service/UserService.java
@@ -1,16 +1,29 @@
 package com.ktds.myspringboot.service;
 
+import com.ktds.myspringboot.dto.UserReqDto;
+import com.ktds.myspringboot.dto.UserResDto;
+import com.ktds.myspringboot.entity.User;
 import com.ktds.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class UserService {
-    private UserRepository userRepository;
-    private ModelMapper modelMapper;
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+//    public UserService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+    public UserResDto saveUser(UserReqDto userReqDto) {
+        //reqDto => entity
+        User user = modelMapper.map(userReqDto, User.class);
+        User savedUser = userRepository.save(user);
+        //entity => resDto
+        return modelMapper.map(savedUser, UserResDto.class);
     }
+
 }
