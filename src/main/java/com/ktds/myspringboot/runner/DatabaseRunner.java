6cd47c09911d9e79af033a20cc3d8393958273a5
myvega2k <vega2k@kakao.com>
2023-08-17 12:45:32 +0900

    dbrunner update

--- src/main/java/com/ktds/myspringboot/runner/DatabaseRunner.java
+++ src/main/java/com/ktds/myspringboot/runner/DatabaseRunner.java
@@ -1,23 +1,32 @@
 package com.ktds.myspringboot.runner;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 
 @Component
+@Order(1)
+@Slf4j
 public class DatabaseRunner implements ApplicationRunner {
     @Autowired
     DataSource dataSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        log.info("DataSource 구현 클래스명 {}",dataSource.getClass().getName());
         try (Connection connection = dataSource.getConnection()) {
-            System.out.println(connection.getMetaData().getURL());
-            System.out.println(connection.getMetaData().getUserName());
+            DatabaseMetaData metaData = connection.getMetaData();
+            log.info("DB Product Name = {}", metaData.getDatabaseProductName());
+            log.info("DB URL = {}",metaData.getURL());
+            log.info("DB Username = {}",metaData.getUserName());
+
         }
     }
 }
