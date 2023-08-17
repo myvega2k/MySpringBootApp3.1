55ef5e16c1aaa410f099dbabae28563273156491
myvega2k <vega2k@kakao.com>
2023-08-17 17:12:40 +0900

    restcontroller add

--- src/main/java/com/ktds/myspringboot/controller/UserRestController.java
+++ src/main/java/com/ktds/myspringboot/controller/UserRestController.java
@@ -1,7 +1,22 @@
 package com.ktds.myspringboot.controller;
 
+import com.ktds.myspringboot.dto.UserReqDto;
+import com.ktds.myspringboot.dto.UserResDto;
+import com.ktds.myspringboot.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserRestController {
+    private final UserService userService;
+
+    @PostMapping
+    public UserResDto saveUser(@RequestBody UserReqDto userReqDto) {
+        return userService.saveUser(userReqDto);
+    }
 }
