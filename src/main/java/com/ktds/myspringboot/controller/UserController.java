a58bff40540c7e8c2458a98e4833993bbf914b72
myvega2k <vega2k@kakao.com>
2023-08-18 14:18:03 +0900

    thymeleaf

--- src/main/java/com/ktds/myspringboot/controller/UserController.java
+++ src/main/java/com/ktds/myspringboot/controller/UserController.java
@@ -1,15 +1,26 @@
 package com.ktds.myspringboot.controller;
 
+import com.ktds.myspringboot.entity.User;
+import com.ktds.myspringboot.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name","스프링부트");
         return "leaf";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("users", userService.getUsers());
+        return "index";
     }
 
 }
