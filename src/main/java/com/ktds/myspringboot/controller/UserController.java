15cfdf0db88adbce8139dad4f94a56f6a9879220
myvega2k <vega2k@kakao.com>
2023-08-18 15:14:51 +0900

    thymeleaf

--- src/main/java/com/ktds/myspringboot/controller/UserController.java
+++ src/main/java/com/ktds/myspringboot/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.ktds.myspringboot.controller;
 
+import com.ktds.myspringboot.dto.UserReqDto;
 import com.ktds.myspringboot.entity.User;
 import com.ktds.myspringboot.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,5 +26,17 @@ public class UserController {
         model.addAttribute("users", userService.getUsers());
         return "index";
     }
-
+    @GetMapping("/signup")
+    public String showSignUpForm(UserReqDto user) {
+        return "add-user";
+    }
+    @PostMapping("/adduser")
+    public String addUser(@Valid UserReqDto user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+        userService.saveUser(user);
+        model.addAttribute("users", userService.getUsers());
+        return "index";
+    }
 }
