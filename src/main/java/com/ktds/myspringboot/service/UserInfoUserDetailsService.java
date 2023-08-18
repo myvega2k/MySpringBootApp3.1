b9170f0d8fd643adf6580047b520f797c732e5ed
myvega2k <vega2k@kakao.com>
2023-08-18 17:09:47 +0900

    security

--- src/main/java/com/ktds/myspringboot/service/UserInfoUserDetailsService.java
+++ src/main/java/com/ktds/myspringboot/service/UserInfoUserDetailsService.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserInfoUserDetailsService implements UserDetailsService {
     @Autowired
