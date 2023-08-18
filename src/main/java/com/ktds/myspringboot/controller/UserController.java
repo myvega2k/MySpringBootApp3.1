9a9c99a94dbaa844aa0d83af63744baa68110d6c
myvega2k <vega2k@kakao.com>
2023-08-18 14:01:44 +0900

    thymeleaf

---
+++ src/main/java/com/ktds/myspringboot/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.ktds.myspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/leaf")
+    public String leaf(Model model) {
+        model.addAttribute("name","스프링부트");
+        return "leaf";
+    }
+
+}
