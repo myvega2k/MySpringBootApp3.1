c0aede44e238fa768fbcdac6da26d86875c0db34
myvega2k <vega2k@kakao.com>
2023-08-18 15:43:03 +0900

    cors

--- src/main/java/com/ktds/myspringboot/config/WebConfig.java
+++ src/main/java/com/ktds/myspringboot/config/WebConfig.java
@@ -1,16 +1,24 @@
 package com.ktds.myspringboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
+    @Override //http://localhost:8080/mobile/index.html
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/mobile/**")
-                //반드시 m 다음에 / 을 주어야 한다.
+                //반드시 mobile 다음에 / 을 주어야 한다.
                 .addResourceLocations("classpath:/mobile/")
                 .setCachePeriod(20);//20초
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:18080")
+                .allowedMethods("*");
     }
 }
