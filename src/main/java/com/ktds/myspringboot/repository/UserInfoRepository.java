3da97e453414cac5ca1bd1e3c29cdb8913651e0a
myvega2k <vega2k@kakao.com>
2023-08-18 17:09:20 +0900

    security

--- src/main/java/com/ktds/myspringboot/repository/UserInfoRepository.java
+++ src/main/java/com/ktds/myspringboot/repository/UserInfoRepository.java
@@ -3,5 +3,8 @@ package com.ktds.myspringboot.repository;
 import com.ktds.myspringboot.entity.UserInfo;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Optional;
+
 public interface UserInfoRepository extends ListCrudRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email);
 }
--- src/main/java/com/ktds/myspringboot/service/UserInfoUserDetailsService.java
+++ src/main/java/com/ktds/myspringboot/service/UserInfoUserDetailsService.java
@@ -19,9 +19,9 @@ public class UserInfoUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<UserInfo> optionalUserInfo = repository.findByName(username);
+        Optional<UserInfo> optionalUserInfo = repository.findByEmail(username);
         return optionalUserInfo.map(userInfo -> new UserInfoUserDetails(userInfo))
-//userInfo.map(UserInfoUserDetails::new)
+                //userInfo.map(UserInfoUserDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException("user not found " + username));
     }
 
